4cc6eda79b2c2508f969fdceb9895becea10380f
Ohtarion <marten.dobelmann@gmail.com>
2026-03-23 12:56:31 +0100

    "Initial commit"

---
+++ main.py
@@ -0,0 +1,10 @@
+'''This is a program that will be an assistant to the game 'Slimerancher 2'. It will be able to provide information about their different diets. '''
+
+
+class Main:
+    def __init__(self):
+        print("Hello, World!")
+
+
+if __name__ == "__main__":
+    Main()
